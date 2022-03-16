fd0fd30941ad47cfa593f5919625a3b0e6e84285
Sir-gits-a-lot <85702060+Sir-gits-a-lot@users.noreply.github.com>
2022-03-16 18:26:28 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	$conn = mysqli_connect("127.0.0.1","root","","sparks_bank");
-
-	if(!$conn){
-		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
-	}
-
-?>
